e3847414cc279d31c1af6db724f802b9ff921b41
Geraint Ballinger <G3zz@users.noreply.github.com>
2022-06-01 12:04:03 +0100

    Fix mkdocs warnings

---
+++ scripts/test.py
@@ -0,0 +1,73 @@
+from sense_hat import SenseHat
+from time import sleep, time
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SECONDS_PER_TEST = 10
+sense = SenseHat()
+
+def measure(seconds_per_test: int) -> None:
+    stop_time = time() + seconds_per_test
+    while time() > stop_time: 
+        sleep(2 * sense.colour.integration_time)
+        red, green, blue, clear = sense.colour.colour # readings scaled to 0-256
+        logger.info(f"R: {red}, G: {green}, B: {blue}, C: {clear}")
+
+
+logger.info("Testing that readings with default values work...")
+try:
+    measure(SECONDS_PER_TEST)
+except Exception as e:
+    logger.error(f"Received {e} while running default test. Quitting...")
+    quit()
+
+# A higher gain setting means the
+# output will be higher for the same input...
+# 
+# There are four possible gain values for the colour sensor: 
+# 1, 4, 16 and 60, with the default value being 1. 
+# You can get or set the sensor gain through the gain property of the ColourSensor object. 
+# An attempt to set the gain to a value that is not valid will result in a ValueError exception being raised.
+
+gain = (1,4,16,60)
+logger.info("Testing setting the gain...") 
+for g in gain:
+    sense.color.gain = g
+    logger.info(f"Gain: {g}")
+    measure(SECONDS_PER_TEST)
+    
+
+invalid_gain = (2, 8, 32)
+logger.info("Testing setting an invalid gain... expecting a ValueError.")
+for g in invalid_gain:
+    try:
+        sense.color.gain = g
+        logger.info(f"Gain: {g}")
+        measure(SECONDS_PER_TEST)
+    except ValueError as e:
+        logger.info(f"Received ValueError {e} as expected!")
+
+
+sense.color.gain=1 # reset
+# Any integer between 1 and 256.
+# The default is 1. Each cycle is 2.4ms long.
+logger.info("Testing setting the number of integration cycles...")
+for num in range(1,257):
+    sense.color.integration_cycle = num
+    logger.info(f"Integration cycle: {num}")
+    measure(SECONDS_PER_TEST)
+
+sense.color.integration_cycle = 2
+invalid = (0,257)
+logger.info("Testing setting an invalid integration cycle.")
+for i in invalid:
+    logger.info(f"Integration cycle: {num}")
+    try:
+        sense.color.integration_cycle = i
+        measure(SECONDS_PER_TEST)
+    except Exception as e:
+        logger.info(f"Recevied {e}")
+
+logger.info("Tests completed")
---
+++ scripts/test_runner.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+arch
+uname -a
+python test.py
